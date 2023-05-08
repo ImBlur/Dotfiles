c65456896279b94e69c5d2b42b0c2a7b09aaaee9
0xBLUR <dariusalexa12@gmail.com>
2023-05-08 19:21:54 +0300

    updates

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo "Installing required packages..."
-sudo pacman -Syy bspwm sxhkd alacritty zsh polybar rofi picom nitrogen vim neovim
+sudo pacman -Syy bspwm sxhkd alacritty zsh polybar rofi picom nitrogen vim neovim xorg-xrandr python
 
 echo "Installing required fonts..."
-sudo pacman -Syy ttf-font-awesome
+sudo pacman -Syy ttf-font-awesome otf-overpass
 
 echo "Installing config..."
 cd ~
 git clone https://github.com/ImBlur/Dotfiles.git
-mv ./Dotfiles/.config ~
-rm -rf ./Dotfiles
+mv ./Dotfiles/.config/ ~
+mv ./Dotfiles/.xinitrc ~
+
+echo "Install oh-my-zsh and then move '~/Dotfiles/.zshrc' to '~'"
+echo "After this you can type 'rm -rf ~/Dotfiles'"
